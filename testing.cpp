d6bf1827ecf06fb7022a381ce1667c17c2ed1d6a
Aidan Ching <aidanching@gmail.com>
2020-11-07 12:35:25 -0800

    Create testing.cpp

---
+++ testing.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Circle.h"
+#using namespace std;
+
+int main() {
+
+
+  cout << "Hello World!" << endl;
+
+  cout << "test" << endl;
+  cout << "test again" << endl;
+  cout << "test again again" << endl;
+  cout << "testing commit" << endl;
+  cout << "HELLO";
+
+
+  for (int i = 0; i < 50; ++i)
+  {
+    cout << "hello";
+  }
+
+  //hello rachel and krystal!!!
+>>>>>>> Stashed changes
+return 0
+
+
+}
